378f085c729095995b87024bd459ad54c5c14158
smag <smag@TEC-5340-LA0131.ADM.NET.LOCAL>
2016-04-04 11:54:30 +0100

    rettet tr og th Indssat pseudo kode i php block

--- index.php
+++ index.php
@@ -1,9 +1,4 @@
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
 <html>
     <head>
         <meta charset="UTF-8">
@@ -11,17 +6,24 @@ and open the template in the editor.
     </head>
     <body>
         <table>
+            <tr>
             <th>
-            <td>
                 Fahrenheit
-            </td>
-            <td>
-                Celsius
-            </td>
             </th>
+            <th>
+                Celsius
+            </th>
+            </tr>
             <?php
-         
-            
+                
+            // gennemløb intavallet fra -50 F til 50 F, i trin af 5 F
+            // Set variablen f til -50
+            // SåLænge f er mindre end eller lig 50
+                // beregng Celsius fra f : ((f - 32) * 5 ) / 9
+                // udskriv f og c i tr og td : <tr><td>f</td><td>c<td></tr>
+                // opskriv f med 5 
+            // slut på SåLænge
+                
             ?>
         </table>
     </body>
